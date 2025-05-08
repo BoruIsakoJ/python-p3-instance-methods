bdea8fad88b48c3f89843589177d557678f93896
Boru Isako <isco.jaldy@gmail.com>
2025-05-08 12:15:05 +0300

    Completed labs

--- lib/person.py
+++ lib/person.py
@@ -4,4 +4,13 @@ class Person:
     # Class body goes here
 
     #Instance method definition
-    pass
+    def talk(self):
+        print("Hello World!")
+    def walk(self):
+        print("The person is walking.")
+        
+person1 = Person()
+print(dir(person1))
+
+person1.walk()
+person1.talk()
